69bb63f72f044510b78d6f078cc68f5f79274c01
[oksana-furman] <[oksan9a.furman@gmail.com]>
2023-02-20 11:58:48 +0100

    create folders, nav, footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeroComponent } from './hero/hero.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HeroComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
